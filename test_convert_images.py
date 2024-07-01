a0f61c7e4859e6cf45332d07d12d76c5d83a2f35
1t4g <90423909+1t4g@users.noreply.github.com>
2024-07-01 16:20:37 +0300

    Update test_convert_images.py

--- test_convert_images.py
+++ test_convert_images.py
@@ -1,45 +1,34 @@
-import os
 import pytest
-from convert_images import convert_images
+import os
+import shutil
+from PIL import Image
 
 @pytest.fixture
 def setup_test_environment():
-    # Создайте временные папки для тестирования
+    # Создаем временные папки для тестирования
     png_folder = "test_png"
     webp_folder = "test_webp"
-    
+
     os.makedirs(png_folder, exist_ok=True)
     os.makedirs(webp_folder, exist_ok=True)
 
-    # Создайте тестовое изображение
-    from PIL import Image
-    img = Image.new('RGB', (1000, 1000), color = 'red')
+    # Создаем тестовое изображение
+    img = Image.new('RGB', (1000, 1000), color='red')
     img.save(os.path.join(png_folder, 'test.png'))
 
     yield png_folder, webp_folder
 
-    # Удалите временные файлы и папки
-    if os.path.exists(os.path.join(png_folder, 'test.png')):
-        os.remove(os.path.join(png_folder, 'test.png'))
-    if os.path.exists(os.path.join(webp_folder, '100px/test100px.webp')):
-        os.remove(os.path.join(webp_folder, '100px/test100px.webp'))
-    if os.path.exists(os.path.join(webp_folder, '512px/test512px.webp')):
-        os.remove(os.path.join(webp_folder, '512px/test512px.webp'))
-    if os.path.exists(os.path.join(webp_folder, '100px')):
-        os.rmdir(os.path.join(webp_folder, '100px'))
-    if os.path.exists(os.path.join(webp_folder, '512px')):
-        os.rmdir(os.path.join(webp_folder, '512px'))
+    # Удаляем временные файлы и папки
     if os.path.exists(png_folder):
-        os.rmdir(png_folder)
+        os.remove(os.path.join(png_folder, 'test.png'))
     if os.path.exists(webp_folder):
-        os.rmdir(webp_folder)
+        shutil.rmtree(webp_folder)  # Рекурсивно удаляем содержимое и саму папку webp_folder
 
 def test_convert_images(setup_test_environment):
     png_folder, webp_folder = setup_test_environment
 
-    # Выполните конвертацию
+    # Выполняем конвертацию
     convert_images(png_folder, webp_folder)
 
-    # Проверьте, что изображения были созданы
+    # Проверяем, что изображения были созданы
     assert os.path.exists(os.path.join(webp_folder, '100px/test100px.webp'))
-    assert os.path.exists(os.path.join(webp_folder, '512px/test512px.webp'))
